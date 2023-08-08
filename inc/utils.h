8f1fe89ac8424a47211933b7e771420e3d18733c
snagulap <snagulap@student.42heilbronn.de>
2023-08-08 18:09:34 +0200

    checked with nor flags

--- inc/utils.h
+++ inc/utils.h
@@ -18,17 +18,13 @@
 # include "parser.h"
 # include "ft_dprintf.h"
 # include "parser.h"
-<<<<<<< HEAD
 
 # include <termios.h>
-=======
->>>>>>> 97468d3b508126543b10eec73f7b4f912ed7f22f
 
 char	*token_str(int type);
 int		token_type(char *str);
 int		is_builtin(char *cmd);
 int		error_msg(int error, char *format, ...);
-<<<<<<< HEAD
 char	*var_value(char *key, t_list *var_list);
 
 void	clear(void *content);
@@ -36,8 +32,5 @@ char	*prompt_line(char *prompt);
 void	setup_termios(struct termios *termios);
 char	*semi_line_iterator(char *line);
 int		semi_syntax_handler(char *line);
-=======
-char	*prompt_line(char *prompt);
->>>>>>> 97468d3b508126543b10eec73f7b4f912ed7f22f
 
 #endif
